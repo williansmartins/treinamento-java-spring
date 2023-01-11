71208e59c7bab0a8d7de1c55be1bc0975b177f8b
Willians Martins <professorwillians@gmail.com>
2023-01-11 20:30:58 -0300

    add primitivo

--- aula2/Primitivos.java
+++ aula2/Primitivos.java
@@ -1,14 +1,24 @@
 public class Primitivos {
     public static void main(String[] args) {
-        byte;
-        short;
-        int idade = 40;
-        long;
-        boolean;
-        char;
-        float;
-        double;
+        byte b = 1;         //8 bits
+        short s = 1;        //16 bits
+        int idade = 40;     //32 bits
+        long l = 1l;        //64 bits
+
+        boolean b2 = true;  //lógico
+        
+        char c = 1;         //caracter
+        
+        float f = 1f;       //32 bits
+        double d = 1d;      //64 bits
 
         System.out.print("Deu certo!");
+
+        //TypeOf -> informa o tipo da classe
+        // 123 typeof -> number
+        // "123" typeof -> String
+
+        //Cast
+        int i = (int) l;
     }
 }
