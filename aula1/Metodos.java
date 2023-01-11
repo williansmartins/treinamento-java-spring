475566f9145f5c87df082907fe8ca99d44f23f2b
Willians Martins <professorwillians@gmail.com>
2023-01-11 20:14:03 -0300

    add metodos

---
+++ aula1/Metodos.java
@@ -0,0 +1,12 @@
+public class Metodos {
+    
+    //criar metodo sem retorno
+    void metodo1(){
+
+    }
+
+    //criar metodo que retorna um inteiro
+    int metodo2(){
+        return 123;
+    }
+}
