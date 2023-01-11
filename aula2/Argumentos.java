48ac24818cd6d88c6a97cf3629fd3e54eccb8c2e
Willians Martins <professorwillians@gmail.com>
2023-01-11 19:17:24 -0300

    add classe

---
+++ aula2/Argumentos.java
@@ -0,0 +1,15 @@
+public class Argumentos {
+    
+    public static void main(String[] args) {
+        metodo1("Willians");
+        metodo2(40);
+    }
+
+    static void metodo1(){
+        System.out.println("metodo1");
+    }
+
+    static void metodo2(){
+        System.out.println("metodo2");
+    }
+}
