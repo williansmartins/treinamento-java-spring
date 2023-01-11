2a85c8d7a8dbf1d4d20714a91bbe11041074ec92
Willians Martins <professorwillians@gmail.com>
2023-01-11 20:07:01 -0300

    add primitivo

--- aula2/Operadores.java
+++ aula2/Operadores.java
@@ -2,14 +2,30 @@ public class Operadores {
     
     public static void main(String[] args) {
         somar(11, 22);
-        subtrair...
-        dividir...
-        multiplicar...
-        mod....
+        subtrair(11, 22);
+        dividir(11, 22);
+        multiplicar(11, 22);
+        mod(11, 22);
     }
 
     static void somar(int a, int b){
         System.out.println("Soma:" + (a+b));
+    }
+    
+    static void subtrair(int a, int b){
+        System.out.println("Subtrair:" + (a-b));
+    }
+
+    static void dividir(int a, int b){
+        System.out.println("Dividir:" + (a/b));
+    }
+    
+    static void multiplicar(int a, int b){
+        System.out.println("Multiplicar:" + (a*b));
+    }
+
+    static void mod(int a, int b){
+        System.out.println("Mod:" + (a%b));
     }
 }
 
---
+++ aula2/Primitivos.java
@@ -0,0 +1,14 @@
+public class Primitivos {
+    public static void main(String[] args) {
+        byte;
+        short;
+        int idade = 40;
+        long;
+        boolean;
+        char;
+        float;
+        double;
+
+        System.out.print("Deu certo!");
+    }
+}
