4c4910cde215a3e32c48ea0f886c9eb87713ecbb
Willians Martins <professorwillians@gmail.com>
2023-01-11 21:26:36 -0300

    add operadores2

---
+++ aula2/Operadores2.java
@@ -0,0 +1,34 @@
+
+public class Operadores2 {
+
+	public static void main(String[] args) {
+		//lógicos (and, or, xor, not)		
+		System.out.println(true & false);
+		System.out.println(true | false);
+		System.out.println(false ^ false);
+		System.out.println(!false);
+		
+		//operadores de bits
+		System.out.println(3 & 10);
+		System.out.println(6 | 5);
+		System.out.println(6 ^ 5);
+		System.out.println(10 >> 2);
+		System.out.println(10 << 2);
+		
+		//relacionais
+		System.out.println(4 > 5);
+		System.out.println(4 >= 5);
+		System.out.println(4 < 5);
+		System.out.println(4 <= 5);
+		System.out.println(4 != 5);
+		System.out.println(4 == 5);
+		
+
+		
+		// 123 * 456 -> NUMERO
+		// 4 > 6 -> BOOLEAN
+		// 4 && 8 -> Operação usando BITS
+
+	}
+
+}
